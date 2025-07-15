f593a4f9d0b89702eb2008503d06e085f093cef5
Simple Guruji <kumaramit957014@gmail.com>
2025-07-15 18:26:53 +0800

    add ignores in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,20 @@ export default [
     rules: {
       // 'no-unused-vars': 'error',
     },
+    ignores: [
+      "dist",
+      "node_modules",
+      "eslint.config.mjs",
+      "jest.config.js",
+      "generateScripts.mjs",
+      "scripts/generateScripts.mjs",
+      "scripts/convertPemToJwk.mjs",
+      "tests",
+      "*.spec.ts",
+      "coverage",
+    ],
   },
+
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
